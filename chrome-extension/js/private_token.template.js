e864c153776ffbd5b564525ca43bb8e7412fb638
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2019-02-18 17:54:41 +0900

    feat: GitLabのMergeRequestでcommitを見たときにコミットメッセージを表示する

---
+++ chrome-extension/js/private_token.template.js
@@ -0,0 +1,5 @@
+/*
+cp private_token.template.js private_token.js
+GitLabからprivate tokenを取得してprivate_token.jsに書き込む
+*/
+window.privateToken = '____________________';
---
+++ chrome-extension/js/show_commit_message.js
@@ -0,0 +1,17 @@
+{
+	const privateToken = window.privateToken;
+
+	const projectId = document.getElementById('search_project_id').value;
+	const commitHash = new URL(document.URL).searchParams.get('commit_id');
+	const url = `/api/v4/projects/${projectId}/repository/commits/${commitHash}?private_token=${privateToken}`;
+
+	fetch(url).then(res => res.json()).then(json => {
+		console.info(json);
+		document.title = json.title;
+
+		const commitMessageElement = document.createElement('pre');
+		commitMessageElement.innerText = json.message;
+		const mrTitleElement = document.querySelector('h2.title');
+		mrTitleElement.parentElement.insertBefore(commitMessageElement, mrTitleElement.nextSibling);
+	});
+}
